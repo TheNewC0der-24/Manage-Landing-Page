55a634d06aa991324160d39aa233ce8ef266b1bd
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-01-25 06:48:16 +0530

    Add functionality -receive an error message when the newsletter sign up form is submitted if: The input field is empty The email address is not formatted correctly

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
+// Variables
 const btn = document.getElementById('menu_btn');
 const nav = document.getElementById('menu');
+const inputForm = document.getElementById('email_input');
+const goBtn = document.getElementById('go_btn');
+const error = document.querySelector('.error');
 
 btn.addEventListener('click', () => {
     btn.classList.toggle('open');
     nav.classList.toggle('flex');
     nav.classList.toggle('hidden');
 });
+
+// Regular Expression for validity of email
+let regex = /^([a-z 0-9\.-]+)@([a-z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})?$/;
+
+// EventListener for form to show error on wrong email format
+goBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (!inputForm.value) {
+        error.classList.add("show");
+        inputForm.classList.add("show");
+        error.innerHTML = "Enter Email";
+        return;
+    }
+    if (!regex.test(inputForm.value)) {
+        error.classList.add("show");
+        inputForm.classList.add("show");
+        error.innerHTML = "Whoops, make sure it's an email";
+        return;
+    }
+
+    error.classList.remove("show");
+    inputForm.classList.remove("show");
+    error.innerHTML = "";
+    inputForm.value = "";
+    return;
+
+})
